33bac268928e1226e1b862ce759c1cb6b8fef10d
Andres <andresgarciamartin1988@gmail.com>
2020-12-22 19:15:10 +0100

    Ejercicio 01

---
+++ js/demo.js
@@ -0,0 +1,4 @@
+// document.getElementById('avatar').style.display = "block";
+document.getElementById('avatar').src = './img/avatar.png';
+document.getElementById('name').value = 'Andrés';
+document.getElementById('lastName').value = 'García';
